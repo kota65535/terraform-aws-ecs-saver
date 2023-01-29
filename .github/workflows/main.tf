834eb40afd4d3e40ba11015eb50d6ef1d5e1aa4e
Tomohiko Ozawa <kota65535@gmail.com>
2023-01-29 15:32:25 +0900

    test

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -25,7 +25,57 @@ resource "aws_ecs_cluster" "main" {
   name = "test"
 }
 
-module "ec2-saver" {
-  source  = "kota65535/ec2-saver/aws"
-  version = "0.0.1"
+resource "aws_iam_role" "ecs" {
+  name               = "ecs-tasks-test"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_trust_relationship.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs" {
+  role       = aws_iam_role.ecs.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_trust_relationship" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "test"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = ["subnet-0abaada26acb8894f"]
+    assign_public_ip = true
+  }
+  tags = {
+    AutoStartTime = 10
+    AutoStopTime  = 11
+  }
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = "test"
+  execution_role_arn       = aws_iam_role.ecs.arn
+  task_role_arn            = aws_iam_role.ecs.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 512
+  memory                   = 1024
+  container_definitions = jsonencode([
+    {
+      name      = "ubuntu"
+      image     = "public.ecr.aws/lts/ubuntu:20.04_stable"
+      essential = true
+      command   = ["tail", "-f"]
+    },
+  ])
 }
