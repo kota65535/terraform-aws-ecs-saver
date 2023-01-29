bd530b4864208e55ef416e8e8e5c4cc273c4b2ff
Tomohiko Ozawa <kota65535@gmail.com>
2023-01-29 14:30:44 +0900

    debug input

--- lambda/src/function.py
+++ lambda/src/function.py
@@ -17,7 +17,11 @@ ecs = boto3.client("ecs")
 def lambda_handler(event: dict, context: dict):
     logger.info(f"Input: {json.dumps(event)}")
 
-    current_hour = str(datetime.now(tz=timezone(TIMEZONE)).hour)
+    if "current_hour" in event:
+        # for debug purpose
+        current_hour = event["current_hour"]
+    else:
+        current_hour = str(datetime.now(tz=timezone(TIMEZONE)).hour)
 
     logger.info(f"current hour: {current_hour}")
 
