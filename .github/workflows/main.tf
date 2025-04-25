f85e14362d74027f692651666e626e81f6a071d9
Tomohiko Ozawa <kota65535@gmail.com>
2025-04-25 19:22:23 +0900

    fix CI

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -7,7 +7,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.52.0"
+      version = "5.96.0"
+    }
+    temporary = {
+      source  = "kota65535/temporary"
+      version = "1.0.1"
     }
   }
   required_version = "~> 1.11.0"
