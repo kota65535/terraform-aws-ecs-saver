3b2ace154d1d636c1017392056282d8522b62d4c
Tomohiko Ozawa <kota65535@gmail.com>
2025-04-26 12:00:41 +0900

    run 3 times per hour

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "rule" {
   name                = var.lambda_name
   description         = "Kick ${var.lambda_name} lambda function every hour"
-  schedule_expression = "cron(0 * * * ? *)"
+  schedule_expression = "cron(0,20,40 * * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "target" {
