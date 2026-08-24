73a16af154c2cf0492e72a7da9cbd6c9e7728068
Tomohiko Ozawa <kota65535@gmail.com>
2026-08-24 10:10:05 +0900

    fix: replace deprecated data.aws_region.current.name with .region (#34)

--- providers.tf
+++ providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
     temporary = {
       source  = "kota65535/temporary"
       version = "~> 1.0"
